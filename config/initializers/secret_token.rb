42ec9aaa41900f2b5b15419cfdd875586451ee9b
Stephanie Datu <stephdatu@gmail.com>
2013-07-29 09:24:54 -0700

    Set up dependencies and run rspec generator

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GrandmasRecipes::Application.config.secret_key_base = 'bc61e0fa2005d805be9d59de45b381c6d0647bf1e9fb4e7d65a94183de65fac4c4886d37a1008028f60e3e0cd551f33b0a7deac78d7561a78e9426fd0c81adfd'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+GrandmasRecipes::Application.config.session_store :cookie_store, key: '_grandmas_recipes_session'
